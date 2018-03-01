af4846963b2e0cc58454d3cd4a49b3074f4be0be
Joao Paulo <joaopaulogse@gmail.com>
2018-03-01 00:44:44 -0300

    testando ...

--- index.teste.js
+++ index.teste.js
@@ -1,41 +1,58 @@
-const request = require('request')
-const cheerio = require('cheerio')
+const request = require("request");
+const cheerio = require("cheerio");
+const _ = require("lodash");
+const $ = html => {
+  return cheerio.load(html);
+};
+const tree = [];
+const getAllTags = obj => {
+  obj.map(tag => {
+    if (tag.type === "tag" || tag.type === "script") {
+      // const pai = !!tag.parent?tag.parent.name:''
+      tree.push(createTree(tag));
+      getAllTags(tag.children);
+    }
+  });
+};
+const createTree = tag => {
+  const tr = {};
+  const allObj = [];
+  tag.children.map(t => {
+    if (t.type === "tag") {
+      allObj.push(t.name);
+    }
+  });
+  tr[tag.name] = allObj;
+  return tr;
+};
 
-const $ = (html) => {
-    return cheerio.load(html)
+const handleTree = (tree = []) => {
+  const obj = _.assign.apply({}, tree);
+  const tags = [];
+//   console.log(obj)
+
+  for(let o in obj){
+      obj[o]
+        .filter(node=>!!node)
+        .map((node,i)=>{
+            tags.push({[o]:{[i]:node}})
+        })
+  }
+  console.log(_.defaultsDeep(...tags))
+
+};
+
+async function crawler(url) {
+  await request(url)
+    .on("data", async data => {
+      await getAllTags($(data.toString())._root.children);
+      handleTree(tree);
+    })
+    .on("error", error => console.error);
 }
-const tree = {}
-const getAllTags = (obj) =>{
-    obj.map(tag => {
-        if(tag.type === 'tag' || tag.type === 'script'){
-            const pai = !!tag.parent?tag.parent.name:''
-            
-            // console.log(!!tag.parent?tag.parent.name:'')
-            
-            createTree(tag)[tag.name].map(t=>{
-                if(t == tag.name){
-                }
-                console.log(t)
-            })
-            // console.log(tag)
-            getAllTags(tag.children)
-        }
-    })
-} 
-const createTree = (tag) =>{
-    const tr = {}
-    const allObj = []
-    tag.children.map(t=>{
-        if(t.type === 'tag'){
-            allObj.push(t.name)
-        }
-    })
-    tr[tag.name] = allObj
-    return tr
+try{
+
+    crawler("https://www.google.com/");
+}catch(e){
+    console.error(e)
 }
-
-request('http://www.example.com')
-    .on('data', data=>{
-        getAllTags($(data.toString())._root.children)
-    })
-
---
+++ scraping.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+
+const handlerHtml = (html)=>{
+    const array = !!html.children ? html.children:[]
+    array.map(e=>{
+        console.log(e)
+        handlerHtml(e)
+    })
+}
+
+const crawler = async () => {
+    
+    const browser = await puppeteer.launch({headless:true})
+    const page = await browser.newPage()
+    
+    await page.goto('http://example.com')
+    const html = await page.$('*')
+
+    const $ = cheerio.load(html);
+
+    
+    
+
+    await browser.close()
+    return $._root
+}
+
+crawler()
+    .then((value)=>{
+        handlerHtml(value)
+    })
+    .catch(e=>{
+        console.error(e)
+    })
